8e55fd086dd962b7f84a3b3f01208c28c54ae0c0
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-07-11 22:47:19 -0400

    Added build test to gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -16,19 +16,41 @@ var watchedBrowserify = watchify(browserify({
     packageCache: {}
 }).plugin(tsify));
 
+var browserify = browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify);
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .on('error', () => {
+            fancy_log();
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+}
+
+function bundle_test() {
+    return browserify
+        .bundle()
+        .on('error', (...args) => {
+            fancy_log(args);
+            process.exitCode = 1;
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+}
+
 gulp.task('copy-html', function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest('dist'));
 });
 
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .on('error', fancy_log)
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist'));
-}
-
+gulp.task('build', gulp.series(gulp.parallel('copy-html'), bundle_test));
 gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
 watchedBrowserify.on('update', bundle);
 watchedBrowserify.on('log', fancy_log);
